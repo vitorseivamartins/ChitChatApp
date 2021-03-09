cfcd34b37fd1465b416428eb4181b35697794f84
Vitor Martins <vitorseivamartins@gmail.com>
2021-03-08 22:26:41 -0300

    Adicionar arquivos de projeto.

---
+++ ChitChatApp/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChitChatApp
+{
+    class Program
+    {
+        private static Dictionary<TcpClient, string> _clients = new Dictionary<TcpClient, string>();
+
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 1302);
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("Server started");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client accepted");
+
+                _clients.Add(client, null);
+                WelcomeUser(client);
+
+                Task.Run(() => ReceiveData(client));
+            
+                //Console.WriteLine("Waiting for a connection.");
+            }
+        }
+
+        private static void WelcomeUser(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            StreamWriter sw = new StreamWriter(client.GetStream());
+            sw.WriteLine("*** Welcome to our chat server. Please provide a nickname:");
+            sw.Flush();
+        }
+
+        static async Task AcceptClient()
+        {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 1302);
+            listener.Start();
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client accepted.");   
+        }
+
+        static async Task ReceiveData(TcpClient client)
+        {
+            while (true)
+            {
+                NetworkStream stream = client.GetStream();
+                //StreamReader sr = new StreamReader(client.GetStream());
+                //StreamWriter sw = new StreamWriter(client.GetStream());
+
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    stream.Read(buffer, 0, buffer.Length);
+                    int recv = 0;
+                    foreach (byte b in buffer)
+                    {
+                        if (b != 0)
+                        {
+                            recv++;
+                        }
+                    }
+
+                    string request = Encoding.UTF8.GetString(buffer, 0, recv);
+
+                    string response = null;
+                    if (_clients[client] == null)
+                    {
+                        SendMessageToClient(client, ValidateUser(client, request));
+                        if (_clients[client] != null)
+                            SendMessageToAllClients(string.Format("{0} has joined #general", 
+                                _clients[client]));
+                    }
+                    else if (request.Contains("/exit"))
+                        ExitUser(client);
+                    else if (request.Contains("/p"))
+                    {
+                        var data = request.Split(" ");
+                        response = string.Format("{0} says to {1}: {2}",
+                            _clients[client],
+                            data[1],
+                            string.Join(" ", data.ToList().Skip(2))
+                            );
+                    }
+                    else
+                        response = string.Format("{0} says: {1}",
+                            _clients[client],
+                            request);
+
+                    if (_clients[client] != null && !string.IsNullOrEmpty(response))
+                        SendMessageToAllClients(response);
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong.");
+                    //sw.WriteLine(e.ToString());
+                }
+            }
+        }
+       
+        private static void ExitUser(TcpClient client)
+        {
+            SendMessageToClient(client, "*** Disconected. Bye!");
+            SendMessageToAllClients(string.Format("{0} has disconected from #general",
+                    _clients[client]));
+            _clients.Remove(client);
+        }
+
+        private static string ValidateUser(TcpClient client, string newUser)
+        {
+            if (_clients.ContainsValue(newUser))
+                return string.Format("*** Sorry, the nickname {0} is already taken. Please choose a differente one:", newUser);
+            else
+            {
+                _clients[client] = newUser;
+                return string.Format("*** You are registered as {0}. Joining #general", newUser);
+            }
+        }
+
+        private static void SendMessageToClient(TcpClient client, string message)
+        {
+            StreamWriter sw = new StreamWriter(client.GetStream());
+            sw.WriteLine(message);
+            sw.Flush();
+        }
+
+        private static void SendMessageToAllClients(string message)
+        {
+            foreach (var item in _clients)
+            {
+                SendMessageToClient(item.Key, message);
+            }
+        }
+    }
+}
---
+++ ChitChatClient/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChitChatClient
+{
+    class Program
+    {
+        private static string _userName = null;
+        private static TcpClient _client = new TcpClient("127.0.0.1", 1302);
+        private static bool _cnxOpen = true;
+
+        static void Main(string[] args)
+        {
+            Task.Run(() => ReceiveData());
+            while (true)
+            {
+                SendData(Console.ReadLine());
+            }
+
+            
+
+            //Console.WriteLine("*** Welcome to our chat server. Please provide a nickname:");
+            //while (_userName == null)
+            //    SetNickName();
+
+            //Task.Run(() => ReceiveData());
+
+            //while (_cnxOpen == true)
+            //    dialog();
+
+            //_client.Close();      
+        }
+
+        static private void dialog()
+        {
+            //connection:
+            try
+            {
+                var message = Console.ReadLine();
+
+                if(message.Contains("/exit"))
+                {
+                    _cnxOpen = false;
+                    Console.WriteLine("*** Disconected. Bye!");
+                    return;
+                }
+
+                SendData(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed to connect...");
+                //goto connection;
+            }
+        }
+
+        static async Task ReceiveData()
+        {
+            while(_cnxOpen)
+            {
+                var response = GetData();
+                Console.WriteLine(response);
+            }
+        }
+
+        static private void SetNickName()
+        {
+            var nickname = Console.ReadLine();
+            SendData("n"+nickname);
+
+            var response = GetData();
+
+            if (response.Substring(0, 1) == "1")
+                _userName = nickname;
+            Console.WriteLine(response.Substring(1, response.Length - 1));         
+        }
+
+        static private void SendData(string data)
+        {
+            byte[] sendData = Encoding.ASCII.GetBytes(data);
+
+            NetworkStream stream = _client.GetStream();
+            stream.Write(sendData, 0, sendData.Length);
+        }
+
+        static private string GetData()
+        {
+            NetworkStream stream = _client.GetStream();
+
+            StreamReader sr = new StreamReader(stream);
+            return sr.ReadLine();
+            //string response = sr.ReadLine();
+            //Console.WriteLine(response);
+        }
+
+        static private void GetDataa()
+        {
+            NetworkStream stream = _client.GetStream();
+
+            StreamReader sr = new StreamReader(stream);
+            //return sr.ReadLine();
+            string response = sr.ReadLine();
+            Console.WriteLine(response);
+        }
+    }
+}
